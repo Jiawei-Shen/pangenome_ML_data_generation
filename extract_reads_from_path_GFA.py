0a0fc096ada822a839adcef8a36d22049d21dde9
Jiawei Shen <shenjiawei0116@gmail.com>
2025-02-18 13:44:21 -0600

    Update extract_reads_from_path_GFA.py

--- extract_reads_from_path_GFA.py
+++ extract_reads_from_path_GFA.py
@@ -102,9 +102,7 @@ def process_read(line, node_info, node_read_map, lock, processed_count, output_j
         }
         mapped_nodes = set()
         # print(read.get("path", {}).get("mapping", []))
-        print("type(node_info)")
-        print("57326217" in node_info)
-
+        
         for mapping in read.get("path", {}).get("mapping", []):
             node_id = str(mapping["position"].get("node_id", ""))
             print("Hello: ", type(node_id), '\n', type(node_info[0]))
