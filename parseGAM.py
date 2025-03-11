89d71613ddbe5ea1a2c986694c0c0c06f3916a2e
Jiawei-Shen <shenjiawei0116@gmail.com>
2025-03-11 15:42:00 -0500

    update

---
+++ parseGAM.py
@@ -0,0 +1,24 @@
+import vg_pb2  # Ensure you have the protobuf definitions
+import gzip
+
+def parse_gam(gam_file):
+    alignments = []
+    with gzip.open(gam_file, "rb") as f:
+        while True:
+            size_bytes = f.read(4)
+            if not size_bytes:
+                break
+            size = int.from_bytes(size_bytes, byteorder="little")
+            data = f.read(size)
+            alignment = vg_pb2.Alignment()
+            alignment.ParseFromString(data)
+            alignments.append(alignment)
+    return alignments
+
+if __name__ == "__main__":
+    gam_file = "input.gam"  # Change this to your actual GAM file path
+    alignments = parse_gam(gam_file)
+
+    # Print some parsed alignments
+    for aln in alignments[:3]:  # Print first 3 alignments
+        print(aln)
