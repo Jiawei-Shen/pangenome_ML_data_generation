c8a6a8c2f6ad0630e4ca0ee8201ab6bb74a3b064
Jiawei Shen <shenjiawei0116@gmail.com>
2025-04-13 21:43:28 -0500

    Update parseGAM_find_unperfect_nodes.py

--- parseGAM_find_unperfect_nodes.py
+++ parseGAM_find_unperfect_nodes.py
@@ -4,6 +4,7 @@ import pickle
 import time
 import vg_pb2
 import concurrent.futures
+import os
 
 
 def read_varint(stream):
@@ -21,30 +22,46 @@ def read_varint(stream):
     return result
 
 
-def is_gzipped(filename):
+def parse_gam_file_groups(filename, expected_tag="GAM"):
     with open(filename, 'rb') as f:
-        return f.read(2) == b'\x1f\x8b'
-
-
-def parse_gam_file_groups(filename, group_size=1000):
-    open_func = open
-    if is_gzipped(filename):
-        raise ValueError("Compressed GAM (.gz) not supported without gzip")
-    with open_func(filename, 'rb') as f:
+        group_number = 0
         while True:
-            group = []
             try:
-                for _ in range(group_size):
-                    msg_len = read_varint(f)
-                    msg = f.read(msg_len)
-                    if not msg:
-                        raise EOFError()
-                    group.append(msg)
+                group_count = read_varint(f)
             except EOFError:
-                if group:
-                    yield group
                 break
-            yield group
+
+            group_number += 1
+            if group_count == 0:
+                continue
+
+            try:
+                tag_size = read_varint(f)
+                tag = f.read(tag_size).decode("utf-8")
+            except (EOFError, UnicodeDecodeError):
+                break
+
+            if tag != expected_tag:
+                # Skip group content
+                for _ in range(group_count - 1):
+                    try:
+                        msg_size = read_varint(f)
+                        f.seek(msg_size, os.SEEK_CUR)
+                    except EOFError:
+                        break
+                continue
+
+            messages = []
+            for _ in range(group_count - 1):
+                try:
+                    msg_size = read_varint(f)
+                    msg_bytes = f.read(msg_size)
+                    if len(msg_bytes) == msg_size:
+                        messages.append(msg_bytes)
+                except EOFError:
+                    break
+
+            yield group_number, messages
 
 
 def process_group_serialized(args):
@@ -55,7 +72,7 @@ def process_group_serialized(args):
         alignment = vg_pb2.Alignment()
         alignment.ParseFromString(msg_bytes)
 
-        node_edit_info = {}  # node_id -> list of is_perfect_chunk (per mapping)
+        node_edit_info = {}  # node_id -> list of is_perfect_chunk
 
         for mapping in alignment.path.mapping:
             node_id = mapping.position.node_id
@@ -64,7 +81,6 @@ def process_group_serialized(args):
 
             is_perfect = True
             for edit in mapping.edit:
-                # If any edit has a sequence (i.e., substitution or insertion) or from_length == 0 (insertion)
                 if edit.sequence or edit.from_length == 0:
                     is_perfect = False
                     break
@@ -77,8 +93,6 @@ def process_group_serialized(args):
         for node_id, chunk_results in node_edit_info.items():
             if node_id not in node_counts:
                 node_counts[node_id] = {"perfect": 0, "not_perfect": 0}
-
-            # All mappings to the node must be perfect for the read to count as "perfect" on that node
             if all(chunk_results):
                 node_counts[node_id]["perfect"] += 1
             else:
@@ -100,7 +114,7 @@ def merge_node_counts(all_counts):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_gam", help="Input GAM file (must be uncompressed)")
+    parser.add_argument("input_gam", help="Input GAM file (varint+GAM-tagged format)")
     parser.add_argument("output_pickle", help="Output .pkl file")
     parser.add_argument("--threads", type=int, default=4, help="Number of threads to use")
     args = parser.parse_args()
@@ -108,7 +122,7 @@ def main():
     start_time = time.time()
     print("Reading alignments and launching threads...")
 
-    grouped_messages = [(i, group) for i, group in enumerate(parse_gam_file_groups(args.input_gam))]
+    grouped_messages = [(i, group) for i, group in parse_gam_file_groups(args.input_gam)]
     with concurrent.futures.ProcessPoolExecutor(max_workers=args.threads) as executor:
         results = list(executor.map(process_group_serialized, grouped_messages))
 
