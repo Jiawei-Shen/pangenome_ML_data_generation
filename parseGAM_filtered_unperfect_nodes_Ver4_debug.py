3c6fdfe1a739d58c77403644fe6418270a779c9c
jiawei <shenjiawei0116@gmail.com>
2025-09-01 23:38:36 -0500

    Update

--- parseGAM_filtered_unperfect_nodes_Ver4_debug.py
+++ parseGAM_filtered_unperfect_nodes_Ver4_debug.py
@@ -154,7 +154,7 @@ def process_alignment(raw_message, wanted_nodes, chrom_filter):
             quality_parts.extend(quality_fragment)
             read_offset += edit_length
 
-        print(alignment.identity, mapping, node_offset, sequence_parts, '\n')
+        print(mapping, node_offset, sequence_parts, '\n')
         # Construct final padded fields
         cigar_string = "".join(cigar_parts)
         seq_final = "".join(sequence_parts).encode().ljust(150, b'\x00')[:150]
