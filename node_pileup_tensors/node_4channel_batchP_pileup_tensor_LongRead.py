e592b8fa1e0d8f77466a7fd3d9458322635dd853
jiawei <shenjiawei0116@gmail.com>
2025-09-06 02:16:54 -0500

    Update

--- node_pileup_tensors/node_4channel_batchP_pileup_tensor_LongRead.py
+++ node_pileup_tensors/node_4channel_batchP_pileup_tensor_LongRead.py
@@ -829,7 +829,7 @@ def main():
                                     args.max_view_reads,
                                     args.view if args.view != -1 else float('inf'))
 
-            if nodes_since_last_report >= 10000 or processed == total_tasks:
+            if nodes_since_last_report >= 100 or processed == total_tasks:
                 elapsed_time = time.time() - batch_start_time
                 rate = nodes_since_last_report / elapsed_time if elapsed_time > 0 else 0.0
                 print(
