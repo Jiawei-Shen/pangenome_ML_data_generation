a000bbdf9ab1a468a54fba6f5c3a37f060303af9
Jiawei-Shen <shenjiawei0116@gmail.com>
2025-09-17 13:01:37 -0500

    update

--- parseGAM_filtered_unperfect_nodes_Ver6CPP.py
+++ parseGAM_filtered_unperfect_nodes_Ver6CPP.py
@@ -128,7 +128,7 @@ def initialize_output_files(stats_path, output_prefix):
         nid = int(node_id_key)
         perfect = int(stat.get("perfect", 0))
         not_perfect = int(stat.get("not_perfect", 0))
-        if (perfect + not_perfect) > 0 and not_perfect > 1 and not_perfect / (perfect + not_perfect) > 0.5:
+        if (perfect + not_perfect) > 0 and not_perfect > 1 and not_perfect / (perfect + not_perfect) > 0.05:
             wanted_nodes.add(nid)
             node_counts[nid] = perfect + not_perfect
             R = int(stat.get("max_read_length", 1) or 1)
